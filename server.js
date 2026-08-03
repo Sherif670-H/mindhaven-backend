d835f38b545c2a327e3d6e82b6798f456eed9db3
Sherif670-H <harunasherif41@gmail.com>
2026-08-02 14:57:53 -1200

    Fix file path casing for Problem model

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-import { Problem } from './models/Problem.js';
+import { Problem } from './models/problem.js';
 
 dotenv.config();
 
